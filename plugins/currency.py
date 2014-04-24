29170a43a67ee268d168cf909994af10b448b040
Mendor <mendor@yuuzukiyo.net>
2014-04-24 16:13:49 +0000

    `currency` for private messages

--- plugins/currency.py
+++ plugins/currency.py
@@ -26,3 +26,26 @@ class pluginExchange(urusai_plugin.MucMessage):
             return response_fmt.format(amount, currencyFrom, amountConverted, currencyTo)
         else:
             return "Invalid request, should be like \"100 USD in EUR\"."
+
+
+class pluginExchange(urusai_plugin.Private):
+    """
+    Exchange conversion (private message plugin).
+    Usage: "ex 100 USD in EUR"
+    """
+    triggers = { "^ex\s": "Exchange" }
+
+    @staticmethod
+    def triggerExchange(fromName, fj, message):
+        api_fmt = "http://rate-exchange.appspot.com/currency?from={0}&to={1}"
+        response_fmt = "{0} {1} = {2} {3}"
+        match = re.match(r"^ex\s([0-9]*).*([a-zA-Z]{3})\s+(to|in)\s+([a-zA-Z]{3})", message)
+        if match:
+            (amount, currencyFrom, currencyTo) = match.group(1, 2, 4)
+            request = api_fmt.format(currencyFrom, currencyTo)
+            data = json.load(urllib2.urlopen(request))
+            amountConverted = data['rate'] * float(amount)
+            return response_fmt.format(amount, currencyFrom, amountConverted, currencyTo)
+        else:
+            return "Invalid request, should be like \"100 USD in EUR\"."
+
