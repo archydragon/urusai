06af4028881ba3630c1d653b24c229e9fc88b03f
Mendor <mendor@yuuzukiyo.net>
2014-04-24 20:54:11 +0000

    non-integer amount fix in `currency`

--- plugins/currency.py
+++ plugins/currency.py
@@ -17,9 +17,11 @@ class pluginExchange(urusai_plugin.MucMessage):
     def triggerExchange(fromName, fj, message):
         api_fmt = "http://rate-exchange.appspot.com/currency?from={0}&to={1}"
         response_fmt = "{0} {1} = {2} {3}"
-        match = re.match(r"^ex\s([0-9]*).*([a-zA-Z]{3})\s+(to|in)\s+([a-zA-Z]{3})", message)
+        match = re.match(r"^ex\s([0-9\,\.]*).*([a-zA-Z]{3})\s+(to|in)\s+([a-zA-Z]{3})", message)
         if match:
             (amount, currencyFrom, currencyTo) = match.group(1, 2, 4)
+            amount = amount.replace(",", ".")
+            return str(amount)
             request = api_fmt.format(currencyFrom, currencyTo)
             data = json.load(urllib2.urlopen(request))
             amountConverted = data['rate'] * float(amount)
@@ -39,9 +41,10 @@ class pluginExchange(urusai_plugin.Private):
     def triggerExchange(fromName, fj, message):
         api_fmt = "http://rate-exchange.appspot.com/currency?from={0}&to={1}"
         response_fmt = "{0} {1} = {2} {3}"
-        match = re.match(r"^ex\s([0-9]*).*([a-zA-Z]{3})\s+(to|in)\s+([a-zA-Z]{3})", message)
+        match = re.match(r"^ex\s([0-9\,\.]*).*([a-zA-Z]{3})\s+(to|in)\s+([a-zA-Z]{3})", message)
         if match:
             (amount, currencyFrom, currencyTo) = match.group(1, 2, 4)
+            amount = amount.replace(",", ".")
             request = api_fmt.format(currencyFrom, currencyTo)
             data = json.load(urllib2.urlopen(request))
             amountConverted = data['rate'] * float(amount)
